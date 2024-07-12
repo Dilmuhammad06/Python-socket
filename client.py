64f316219c2ff3f4fee000b0782acc594d07f041
Dilmuhammad Abdukodirov <145844965+Dilmuhammad06@users.noreply.github.com>
2024-07-13 00:45:35 +0500

    Update client.py

--- client.py
+++ client.py
@@ -2,13 +2,19 @@ import socket
 import os
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),12343))
+s.connect(("127.0.0.1",12343)) # use socket.gethostname() to use in local network
+
+NOTALLOWED = ['cd ..', 'cd /']
 
 
 while True:
     msg = str(s.recv(1024).decode("utf-8"))
     print(msg)
     print("Got it...")
-    terminal = os.popen(msg).read()
-    s.send(bytes(terminal,"utf-8"))
-    print("Done.")
+    if msg not in NOTALLOWED:
+        terminal = os.popen(msg).read()
+        s.send(bytes(terminal,"utf-8"))
+        print("Done.")
+    else:
+        print("Sorry I can't")
+        s.send(bytes("Sorry I can't","utf-8"))
