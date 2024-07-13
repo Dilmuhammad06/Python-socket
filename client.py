18d076e473e71362af1c2427748856af0c987ab6
Dilmuhammad Abdukodirov <145844965+Dilmuhammad06@users.noreply.github.com>
2024-07-13 13:37:53 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
 import socket
 import os
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("127.0.0.1",12343)) # use socket.gethostname() to use in local network
+MY_IP = input("IP: ")
+MY_PORT = input("PORT: ")
 
-NOTALLOWED = ['cd ..', 'cd /']
+if len(str(MY_IP)) > 0 and len(str(MY_PORT)) > 0:
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((MY_IP,int(MY_PORT)))
+
+    NOTALLOWED = ['cd ..', 'cd /']
 
 
-while True:
-    msg = str(s.recv(1024).decode("utf-8"))
-    print(msg)
-    print("Got it...")
-    if msg not in NOTALLOWED:
-        terminal = os.popen(msg).read()
-        s.send(bytes(terminal,"utf-8"))
-        print("Done.")
-    else:
-        print("Sorry I can't")
-        s.send(bytes("Sorry I can't","utf-8"))
+    while True:
+        msg = "echo output: && " + str(s.recv(1024).decode("utf-8"))
+        print(msg)
+        print("Got it...")
+        if msg not in NOTALLOWED:
+            terminal = os.popen(msg).read()
+            s.send(bytes(terminal,"utf-8"))
+            print("Done.")
+        else:
+            print("Sorry I can't")
+            s.send(bytes("Sorry I can't","utf-8"))
+
+else:
+    print("There is a problem with IP or PORT")
